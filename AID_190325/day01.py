cd75e96d729d5fa9d121a0267b278f0929d7a4b9
GullomJ <jiangzudong6113@sina.com>
2019-03-25 23:24:43 +0800

    AID_190325/

---
+++ AID_190325/day01.py
@@ -0,0 +1,91 @@
+"""
+print("我说："你真帅"")
+print('我说："你真帅"')
+print('''
+我说:你真帅，
+你回到：一般般
+''')
+
+print("a","b")
+print("a","b",sep='',end='***')
+print("a","b",sep='',end='\n')
+# print("a","b",sep=' ',end='\n')
+"""
+
+# n=input("请输入:")
+# print(n)
+# print(type(1))
+
+
+
+# print("  *")
+# print(" ***")
+# print("*****")
+
+# print("""
+#   *
+#  ***
+# *****
+# """)
+
+# n=input("请输入成绩:")
+# n=int(n)
+# if n>=60:
+#     print("及格")
+# else:
+#     print("不及格")
+
+
+
+# n=input("请输入成绩:")
+# n=int(n)
+# if n>=90:
+#     print("A")
+# elif n>=80:
+#     print("B")
+# elif n>=60:
+#     print("C")
+# else:
+#     print("不及格")
+
+# n=int(n)
+# if n>=90:
+#     print("A")
+# if n>=80:
+#     print("B")
+# if n>=60:
+#     print("C")
+# else:
+#     print("不及格")
+
+
+# n=input("请输入秒数:")
+# n=int(n)
+
+# n=int(input("请输入秒数:"))
+# h=n//3600
+# m=n%3600//60
+# s=n%60
+# print(h,"时",m,"分",s,"秒",sep='')
+
+# n=int(input("请输入月份:"))
+# if 12>=n>0:
+#     if 3<=n<=5:
+#         print("春季")
+#     elif 6<=n<=8:
+#         print("夏季")
+#     elif 9<=n<=11:
+#         print("秋季")
+#     else:
+#         print("冬季")
+# else:
+#     print("输入错误")
+
+
+
+
+
+
+
+
+
