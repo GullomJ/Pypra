16bf0e141378eb179f5e321364a810742729ad47
GullomJ <jiangzudong6113@sina.com>
2019-04-01 23:25:03 +0800

    An exercise

---
+++ day05.py
@@ -0,0 +1,25 @@
+"""
+
+import random
+random.choice(L)
+
+"""
+import random
+
+while True:
+    l = ['Rock', 'Scissors', 'Paper ']
+    gz = [["Rock", "Scissors"], ["Paper ", "Rock"], ["Scissors", "Paper "]]
+    s = random.choice(l)
+    print(s)
+    n = input("play finger-guessing game:")
+    if n == 'q':
+        break
+    if n in l:
+        if n == s:
+            print("draw")
+        elif [n, s] in gz:
+            print("win")
+        else:
+            print("lose")
+    else:
+        print("!")
